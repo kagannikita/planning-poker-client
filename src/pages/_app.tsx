c08a7dc188b886025679304aeb2fbeab2e8aa6c1
Maxim Yazykov <yazykovmaxim@gmail.com>
2021-09-10 18:22:55 +0300

    feat: working with redux-wrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,15 +6,15 @@ import { useRouter } from 'next/router'
 import 'semantic-ui-css/semantic.min.css'
 import TransitionLayout from '../components/layout/Layout'
 import { Provider } from 'react-redux'
-import { initialiseStore, RootState, useStore } from 'src/store/store'
+import { reduxWrapper, RootState, store } from 'src/store/store'
 import { GetServerSideProps } from 'next'
+import withRedux from "next-redux-wrapper";	
 import { setPlayerID } from 'src/store/playerData'
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
 	const router = useRouter()
-	const store = useStore(pageProps.initialReduxState)
 	return (
-		<Provider store={store} >
+		<Provider store={pageProps.store} >
 		<TransitionLayout>
 			<div style={{ position: 'relative' }}>
 				{router.pathname !== '/404' ? <Component {...pageProps} /> : <Component {...pageProps} />}
@@ -24,16 +24,31 @@ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
 	)
 }
 
+
+
 interface AppSSRProps {
 	initialReduxState: RootState
 }
 
-export const getServerSideProps: GetServerSideProps<AppSSRProps> = async () => {
-	const reduxStore = initialiseStore({})
-	// reduxStore.dispatch(setPlayerID(''))
+// export const getServerSideProps: GetServerSideProps<AppSSRProps> = async () => {
+// 	const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
+// 	const reduxStore = initialiseStore({})
+// 	// reduxStore.dispatch(setPlayerID(''))
 
 
-	return { props: { initialReduxState: reduxStore.getState() } }
+// 	return { props: { initialReduxState: reduxStore.getState() } }
+// }
+export const getServerSideProps = reduxWrapper.getServerSideProps(store =>{
+return	{props: store.getState() }
+
 }
+	// ({ req, res, ...etc }) => {
+	// 	console.log('2. Page.getServerSideProps uses the store to dispatch things');
+	// 	store.dispatch({ type: 'TICK', payload: 'was set in other page' });
+	// }
+);
 
-export default MyApp
+const makeStore = () => store;
+
+export default withRedux(makeStore)(MyApp);
+
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,57 +1,30 @@
-import { configureStore, Store } from '@reduxjs/toolkit'
+import { Action, configureStore, Store, ThunkAction } from '@reduxjs/toolkit'
 import { postSlice, PostState } from './posts'
 import playerSlice from './playerData'
-import { useMemo } from 'react';
+import { createWrapper } from 'next-redux-wrapper';
 
-let store: Store;
-
-const createStore = (preloadedState: RootState) => {
-		return configureStore({
-			reducer: {
-				posts: postSlice.reducer,
-				player: playerSlice.reducer
-			},
-			middleware: (getDefaultMiddleware) =>
-				getDefaultMiddleware({
-					thunk: true,
-				}),
-			devTools: process.env.NODE_ENV !== 'production',
-			preloadedState
-		})
-
-	}
-	 
-
-export const initialiseStore = (preloadedState: RootState) => {
-	let _store = store ?? createStore(preloadedState);
-
-	if (preloadedState && store) {
-		_store = createStore({ ...store.getState(), ...preloadedState });
-		// store = undefined
-	}
-
-	// For SSG and SSR always create a new store
-	if (typeof window === 'undefined') return _store;
-	// Create the store once in the client
-	if (!store) store = _store;
-
-	return _store;
-};
-
-export function useStore(initialState: RootState) {
-	const store = useMemo(() => initialiseStore(initialState), [initialState])
-	return store
-}
+export const makeStore = () => configureStore({
+	reducer: {
+		posts: postSlice.reducer,
+		player: playerSlice.reducer
+	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			thunk: true,
+		}),
+	devTools: process.env.NODE_ENV !== 'production',
+})
 
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type AppStore = ReturnType<typeof makeStore>
+export type AppState = ReturnType<AppStore['getState']>;
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppState, unknown, Action>;
 
-export const getPosts: (state: RootState) => {
-	loading: boolean
-	data: PostState | {}
-} = (state: RootState) => state.posts
+// export const someAction = (whatever: any): AppThunk => async dispatch => {
+// 	dispatch(
+// 		subjectSlice.actions.setWhatever({ whatever }),
+// 	);
+// };
 
-// export const getPlayerID: (state:  RootState) => {
-// 	playerID: 
-// }
+
+export  const reduxWrapper = createWrapper<AppStore>(makeStore);
