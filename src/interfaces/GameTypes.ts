4dc1370685d3af648b263e74ca63397d4b4a5cdd
Maxim Yazykov <yazykovmaxim@gmail.com>
2021-09-29 04:05:22 +0300

    feat: implement socket functions for game logic

--- src/interfaces/GameTypes.ts
+++ src/interfaces/GameTypes.ts
@@ -1,11 +1,14 @@
 export enum GameState {
   init = 'init',
   paused = 'paused',
-  started = 'started'
+  started = 'started',
+  roundFinished = 'round-finished',
+  roundRepeat = 'round-repeat'
 } 
 
 export interface GameData {
   status: GameState,
+  playersScore: any,
   currIssueId: string,
   issueScore: number,
   timer: number
