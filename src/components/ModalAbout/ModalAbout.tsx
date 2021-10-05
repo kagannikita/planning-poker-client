d98249c3286ca8985921e5bb530e84459524a46e
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2021-10-05 21:11:18 +0300

    feat: add new item to about

--- src/components/ModalAbout/ModalAbout.tsx
+++ src/components/ModalAbout/ModalAbout.tsx
@@ -6,6 +6,7 @@ import logoPic from '../../../public/images/main_logo.png'
 import mainFormPic from '../../../public/images/about_mainForm-pic.png'
 import formLinkPic from '../../../public/images/about__mainForm-link.png'
 import issuePic from '../../../public/images/about_lobbyIssue-pic.png'
+import issueFilePic from '../../../public/images/about_lobbyIssueFile-pic.png'
 import settingsPic from '../../../public/images/about_lobbySettings-pic.png'
 
 type ModalAboutType = {
@@ -35,7 +36,6 @@ const ModalAbout: FC<ModalAboutType> = ({ aboutModalState, setAboutModalState })
 					</Modal.Description>
 				</div>
 				<div className={cls.modalItem}>
-					<Image className={cls.itemPic} size="large" src={mainFormPic.src} wrapped />
 					<Modal.Description>
 						<p className={cls.itemDescription}>To create a new game session, use the start game button [1]</p>
 						<p className={cls.itemDescription}>
@@ -43,6 +43,7 @@ const ModalAbout: FC<ModalAboutType> = ({ aboutModalState, setAboutModalState })
 							button [3]
 						</p>
 					</Modal.Description>
+					<Image className={cls.itemPic} size="large" src={mainFormPic.src} wrapped />
 				</div>
 				<div className={cls.modalItem}>
 					<Image className={cls.itemPic} size="large" src={formLinkPic.src} wrapped />
@@ -51,20 +52,26 @@ const ModalAbout: FC<ModalAboutType> = ({ aboutModalState, setAboutModalState })
 					</Modal.Description>
 				</div>
 				<div className={cls.modalItem}>
-					<Image className={cls.itemPic} size="large" src={issuePic.src} wrapped />
 					<Modal.Description>
 						<p className={cls.itemDescription}>
 							You can add your issues manually [1] or use the automatic addition of issues from the file [2]
 						</p>
 					</Modal.Description>
+					<Image className={cls.itemPic} size="large" src={issuePic.src} wrapped />
 				</div>
 				<div className={cls.modalItem}>
-					<Image className={cls.itemPic} size="large" src={settingsPic.src} wrapped />
+					<Image className={cls.itemPic} size="large" src={issueFilePic.src} wrapped />
+					<Modal.Description>
+						<p className={cls.itemDescription}>The contents of the issues file should be in this format</p>
+					</Modal.Description>
+				</div>
+				<div className={cls.modalItem}>
 					<Modal.Description>
 						<p className={cls.itemDescription}>Set your settings for the game [1]</p>
 						<p className={cls.itemDescription}>Choose a card cover or upload your own [2]</p>
 						<p className={cls.itemDescription}>Edit the game cards to fit your goals [3]</p>
 					</Modal.Description>
+					<Image className={cls.itemPic} size="large" src={settingsPic.src} wrapped />
 				</div>
 			</Modal.Content>
 			<Modal.Actions>
