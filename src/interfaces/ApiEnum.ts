1060dbde6a43c2e03ef559768d89180fdfca8ea9
Max <72974641+Nectire@users.noreply.github.com>
2021-10-06 22:16:23 +0300

    feat: add vercel link

--- src/interfaces/ApiEnum.ts
+++ src/interfaces/ApiEnum.ts
@@ -1,5 +1,6 @@
 export enum API {
 	FRONT_LINK = 'http://localhost:3000',
+	// FRONT_LINK = 'https://planning-poker-client.vercel.app/',
 	// MAIN_API = 'http://localhost:8080/',
 	MAIN_API='https://nestjs-planning-poker.herokuapp.com/',
 	LOBBY = 'lobby/',
