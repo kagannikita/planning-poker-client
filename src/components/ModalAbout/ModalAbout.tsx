1b63f90fb763eec0a5cf58857f6757e70aee7059
Alexandr Demchenko <76862030+Vitebsk121@users.noreply.github.com>
2021-10-06 11:13:54 +0300

    feat: finish about

--- src/components/ModalAbout/ModalAbout.tsx
+++ src/components/ModalAbout/ModalAbout.tsx
@@ -8,6 +8,8 @@ import formLinkPic from '../../../public/images/about__mainForm-link.png'
 import issuePic from '../../../public/images/about_lobbyIssue-pic.png'
 import issueFilePic from '../../../public/images/about_lobbyIssueFile-pic.png'
 import settingsPic from '../../../public/images/about_lobbySettings-pic.png'
+import gameMenuPic from '../../../public/images/about_gameMenu.png'
+import gameCards from '../../../public/images/about_gameCards.png'
 
 type ModalAboutType = {
 	aboutModalState: TAboutModalState
@@ -72,6 +74,35 @@ const ModalAbout: FC<ModalAboutType> = ({ aboutModalState, setAboutModalState })
 						<p className={cls.itemDescription}>Edit the game cards to fit your goals [3]</p>
 					</Modal.Description>
 					<Image className={cls.itemPic} size="large" src={settingsPic.src} wrapped />
+				</div>
+				<div className={cls.modalItem}>
+					<Image className={cls.itemPic} size="large" src={gameMenuPic.src} wrapped />
+					<Modal.Description>
+						<p className={cls.itemDescription}>Your Issues are available for management here [1]</p>
+						<p className={cls.itemDescription}>These are the game controls [2]</p>
+						<p className={cls.itemDescription}>
+							This is the field with the results of the game. After the end of the game you will be able to download the
+							results in excel format [3]
+						</p>
+						<p className={cls.itemDescription}>
+							This is a field with players in the game. Here you can vote to exclude a player from the game (if there
+							are 4 or more players) or if you are a dealer, immediately exclude a player. [4]
+						</p>
+					</Modal.Description>
+				</div>
+				<div className={cls.modalItem}>
+					<Modal.Description>
+						<p className={cls.itemDescription}>
+							When the round starts, select the card you want or you can skip a move. When all the players vote or the
+							time of the round ends, you will see the average voting results.
+						</p>
+					</Modal.Description>
+					<Image className={cls.itemPic} size="large" src={gameCards.src} wrapped />
+				</div>
+				<div className={[cls.modalItem, cls.modalFooter].join(' ')}>
+					<Modal.Description>
+						<p className={cls.modalDescription}>GOOD LUCK!</p>
+					</Modal.Description>
 				</div>
 			</Modal.Content>
 			<Modal.Actions>
